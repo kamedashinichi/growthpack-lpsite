b2e1aa676947a0782db3c7b673e2a00be89cb574
kamedashinichi <kameda.shinichi@classmethod.jp>
2026-04-13 18:50:07 +0900

    旧URL(?industry=apparel等)から新URL(/apparel等)への301リダイレクト追加

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,7 @@ const nextConfig = {
   },
   async redirects() {
     return [
+      // v2からの移行リダイレクト
       {
         source: '/v2/:path*',
         destination: '/:path*',
@@ -18,6 +19,15 @@ const nextConfig = {
         destination: '/',
         permanent: true,
       },
+      // 旧版クエリパラメータからのリダイレクト
+      { source: '/', has: [{ type: 'query', key: 'industry', value: 'apparel' }], destination: '/apparel', permanent: true },
+      { source: '/', has: [{ type: 'query', key: 'industry', value: 'department' }], destination: '/department', permanent: true },
+      { source: '/', has: [{ type: 'query', key: 'industry', value: 'drugstore' }], destination: '/drugstore', permanent: true },
+      { source: '/', has: [{ type: 'query', key: 'industry', value: 'ec' }], destination: '/ec', permanent: true },
+      { source: '/', has: [{ type: 'query', key: 'industry', value: 'food' }], destination: '/food', permanent: true },
+      { source: '/', has: [{ type: 'query', key: 'industry', value: 'hotel' }], destination: '/hotel', permanent: true },
+      { source: '/', has: [{ type: 'query', key: 'industry', value: 'sports' }], destination: '/sports', permanent: true },
+      { source: '/', has: [{ type: 'query', key: 'industry', value: 'supermarket' }], destination: '/supermarket', permanent: true },
     ]
   },
 }
